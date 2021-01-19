de6038ae57c9b4e3d0e251cac941f21e12558d94
李铭昕 <715557344@qq.com>
2021-01-19 19:13:25 +0800

    Fixed exception

--- src/Calculater/Adapter/Sumer.php
+++ src/Calculater/Adapter/Sumer.php
@@ -9,19 +9,19 @@ declare(strict_types=1);
  */
 namespace Know\Calculater\Adapter;
 
-use Exception;
 use Know\Calculater\Adapter;
+use Know\Calculater\Exceptions\CalculaterException;
 
 class Sumer extends Adapter
 {
     public function handle()
     {
         if (! isset($this->arguments[0]) || ! isset($this->arguments[1])) {
-            throw new Exception('求和算法 必须传入初始值和终止值');
+            throw new CalculaterException('求和算法 必须传入初始值和终止值');
         }
 
         if (! is_numeric($this->arguments[0]) || ! is_numeric($this->arguments[1])) {
-            throw new Exception('求和算法 初始值和终止值必须为纯数字');
+            throw new CalculaterException('求和算法 初始值和终止值必须为纯数字');
         }
 
         $begin = intval($this->arguments[0]);
