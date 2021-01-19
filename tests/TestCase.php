46d786ba6fb87a76b7e2c7f22f6c190389b58d07
李铭昕 <715557344@qq.com>
2021-01-19 19:51:19 +0800

    Removed useless comments

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,13 +7,6 @@ declare(strict_types=1);
  * @contact  l@hyperf.io
  * @license  https://github.com/kyknow/calculater/blob/master/LICENSE
  */
-// | TestCase.php [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
-// +----------------------------------------------------------------------
-
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as UnitTestCase;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,12 +7,6 @@ declare(strict_types=1);
  * @contact  l@hyperf.io
  * @license  https://github.com/kyknow/calculater/blob/master/LICENSE
  */
-// | bootstrap.php [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
-// +----------------------------------------------------------------------
 require __DIR__ . '/../vendor/autoload.php';
 
 define('TESTS_PATH', __DIR__);
